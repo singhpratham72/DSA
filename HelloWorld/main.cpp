fa28384728de970e36c52322846c978288f6fc92
Pratham Singh <singhpratham72@gmail.com>
2021-05-18 22:02:28 +0530

    180521

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,27 +1,29 @@
 // First C++ program
-#include<iostream> // Header file library that lets you work with input and output objects
+#include <iostream>  // Header file library that lets you work with input and output objects
 using namespace std; // We can now use names for objects and variables from the standard library
+// Can use std::cout in place of this ^
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
 
-int main(){
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
-
-    int a,b;
-    cin>>a>>b; // used to input values
-    cout<<a+b<<"\n"; // used to output values
+    int a, b;
+    cin >> a >> b;         // used to input values
+    cout << a + b << "\n"; // used to output values
+    // << insertion operator >> exertion operator
 
     int c;
-    c = 12;
+    c = 15;
 
-    cout<<"Size of int "<<sizeof(c);
-    
+    cout << "Size of int " << sizeof(c);
+
     // \n = nextLine. Can also use endl to inert new line
     // << insertion operator
     /* This
     is a
     multiline comment*/
-    
+
     return 0;
 }
